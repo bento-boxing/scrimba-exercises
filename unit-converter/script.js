5e29316c3c890d779852368164e1f5fd04639bc9
bento-boxing <superflyer7676@gmail.com>
2026-08-17 14:58:07 +0100

    Refactored renderConversions() method in script.js

--- unit-converter/script.js
+++ unit-converter/script.js
@@ -13,14 +13,19 @@ convertBtn.addEventListener('click', renderConversions)
 function renderConversions () {
     let userInput = unitInput.value
     const unitContainers = document.getElementsByClassName('unit-container')
-    if (Number(userInput)) {
+    if (validateInput(userInput)) {
         userInput = Number(userInput)
 
         for(let i = 0; i < unitContainers.length; i++) {
             const unitParagraph = unitContainers[i].querySelector('p')
+
+            const metricName = unitParagraph.dataset.metric
+            const imperialName = unitParagraph.dataset.imperial
+            const multiplier = Number(unitParagraph.dataset.multiplier)
+
             unitParagraph.textContent =
-                `${userInput} ${unitParagraph.dataset.metric} = ${roundNumber(userInput * unitParagraph.dataset.multiplier, 3)} ${unitParagraph.dataset.imperial} |
-                 ${userInput} ${unitParagraph.dataset.imperial} = ${roundNumber(userInput / unitParagraph.dataset.multiplier, 3)} ${unitParagraph.dataset.metric}`
+                `${userInput} ${metricName} = ${forwardConversion(userInput, multiplier, 3)} ${imperialName} | 
+                ${userInput} ${metricName} = ${reverseConversion(userInput, multiplier, 3)} ${imperialName}`
         }
     } else {
         window.alert("Invalid input")
@@ -31,4 +36,16 @@ function roundNumber (number, scale) {
     const sign = Math.sign(number) >= 0 ? 1 : -1
     const abs = Math.abs(number)
     return +(sign * (Math.round(abs + 'e+' + scale) + 'e-' + scale))
+}
+
+function validateInput(input) {
+    return !isNaN(input) && input.trim().length > 0
+}
+
+function forwardConversion(number, multiplier, scale) {
+    return roundNumber(number * multiplier, scale)
+}
+
+function reverseConversion(number, multiplier, scale) {
+    return roundNumber(number / multiplier, scale)
 }
