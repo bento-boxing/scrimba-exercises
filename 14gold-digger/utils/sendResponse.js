efb9b419d3ff218a99ca2d1db8247be37634d485
bento-boxing <superflyer7676@gmail.com>
2026-09-07 17:18:53 +0100

    fix: sendResponse.js ends with the body object instead of stringified object

--- 14gold-digger/utils/sendResponse.js
+++ 14gold-digger/utils/sendResponse.js
@@ -1,5 +1,5 @@
 export function sendResponse(res, statusCode, contentType, body) {
     res.setHeader('Content-Type', contentType)
     res.statusCode = statusCode
-    res.end(JSON.stringify(body))
+    res.end(body)
 }
