7a9498aa8f87d7a64ed2a8fb1b6a7cf0697bccf9
bento-boxing <superflyer7676@gmail.com>
2026-09-07 16:57:46 +0100

    feat: add utility function to serve static files of the website. Add mime-types dependency to make it easier

---
+++ 14gold-digger/utils/serveStatic.js
@@ -0,0 +1,22 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import mime from "mime-types";
+import {sendResponse} from "./sendResponse.js";
+
+export async function serveStatic(req, res, baseDir) {
+    const publicPath = path.join(baseDir, "public")
+    const filePath = path.join(
+        publicPath,
+        req.url === '/' ? 'index.html' : req.url
+    )
+
+    const ext = path.extname(filePath)
+    const contentType = mime.lookup(ext)
+
+    try {
+        const content = await fs.readFile(filePath)
+        sendResponse(res, 200, contentType, content)
+    } catch (err) {
+        console.error(err)
+    }
+}
