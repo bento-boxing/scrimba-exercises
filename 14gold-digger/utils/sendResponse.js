025780dc2204876f244fbe5c9e353cd717bcb5b3
bento-boxing <superflyer7676@gmail.com>
2026-09-07 16:56:47 +0100

    feat: add sendResponse utility function to reduce code duplication

---
+++ 14gold-digger/utils/sendResponse.js
@@ -0,0 +1,5 @@
+export function sendResponse(res, statusCode, contentType, body) {
+    res.setHeader('Content-Type', contentType)
+    res.statusCode = statusCode
+    res.end(JSON.stringify(body))
+}
