1a5a2cb10238d620a204e7b5e8760846e0822cb0
bento-boxing <superflyer7676@gmail.com>
2026-09-08 13:35:34 +0100

    feat: add server sent events to update price every 5 seconds.

--- 14gold-digger/public/index.js
+++ 14gold-digger/public/index.js
@@ -1,6 +1,8 @@
 const investForm = document.getElementById('invest-form')
 const outputDialog = document.querySelector('.outputs')
 const outputButton = document.querySelector('.outputs button')
+const eventSource = new EventSource('/api/gold-price')
+const priceDisplay = document.getElementById('price-display')
 
 investForm.addEventListener('submit', async e => {
     e.preventDefault()
@@ -14,3 +16,11 @@ outputButton.addEventListener('click', e => {
     e.preventDefault()
     outputDialog.close()
 })
+
+eventSource.addEventListener('price-updated', e => {
+    priceDisplay.textContent = e.data
+})
+
+eventSource.onerror = event => {
+    console.log('event error: ', event)
+}
--- 14gold-digger/server.js
+++ 14gold-digger/server.js
@@ -1,7 +1,9 @@
 import http from 'node:http'
 import {serveStatic} from "./utils/serveStatic.js";
+import {getPrice} from "./utils/getPrice.js";
 
 const __dirname = import.meta.dirname
+let currentPrice = 3257.88
 const PORT = 8080
 
 const server = http.createServer(async (req, res) => {
@@ -9,6 +11,19 @@ const server = http.createServer(async (req, res) => {
         if (req.method === 'GET') {
 
         }
+    } else if (req.url === '/api/gold-price') {
+        res.statusCode = 200
+        res.setHeader("Content-Type", "text/event-stream")
+        res.setHeader("Cache-Control", "no-cache")
+        res.setHeader("Connection", "keep-alive")
+
+        setInterval(() => {
+            const newPrice = getPrice(currentPrice)
+            res.write(
+                `event: price-updated\ndata: ${newPrice}\n\n`
+            )
+
+        }, 5000)
     } else {
         return await serveStatic(req, res, __dirname)
     }
