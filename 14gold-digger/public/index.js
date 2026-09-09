febc9b32df343c6f73121e216ab3178514381568
bento-boxing <superflyer7676@gmail.com>
2026-09-09 19:43:45 +0100

    feat: format and update modal to display price and quantity bought.

--- 14gold-digger/public/index.js
+++ 14gold-digger/public/index.js
@@ -3,15 +3,27 @@ const outputDialog = document.querySelector('.outputs')
 const outputButton = document.querySelector('.outputs button')
 const eventSource = new EventSource('/api/gold-price')
 const priceDisplay = document.getElementById('price-display')
-const formatter = new Intl.NumberFormat('en-GB', {
+const investmentWeight = document.getElementById('investment-weight')
+const investmentPrice = document.getElementById('investment-price')
+
+const fiveDPFormatter = new Intl.NumberFormat('en-GB', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 5,
+})
+const gbpFormatter = new Intl.NumberFormat('en-GB', {
     style: 'currency',
     currency: 'GBP'
 })
+
+let currentPrice = 0
 
 investForm.addEventListener('submit', async e => {
     e.preventDefault()
     const formData = new FormData(investForm)
     console.log(formData.get('investment-amount'))
+
+    investmentPrice.textContent = gbpFormatter.format(Number(formData.get('investment-amount')))
+    investmentWeight.textContent = fiveDPFormatter.format(Number(formData.get('investment-amount') / currentPrice))
 
     outputDialog.showModal()
 })
@@ -23,7 +35,8 @@ outputButton.addEventListener('click', e => {
 
 eventSource.addEventListener('price-updated', e => {
     const price = parseInt(e.data) / 100
-    priceDisplay.textContent = formatter.format(price)
+    currentPrice = price
+    priceDisplay.textContent = gbpFormatter.format(price)
 })
 
 eventSource.onerror = event => {
