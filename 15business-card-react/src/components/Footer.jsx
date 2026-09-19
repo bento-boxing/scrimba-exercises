671d9cc8cd67623ec9dc91c8cabe679d7ed57c07
bento-boxing <superflyer7676@gmail.com>
2026-09-19 18:05:17 +0100

    feat: Create Footer.jsx

---
+++ 15business-card-react/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export function Footer() {
+    return (
+        <footer className="footer">
+            <i className="fa-brands fa-instagram"></i>
+            <i className="fa-brands fa-facebook-f"></i>
+            <i className="fa-brands fa-twitter"></i>
+            <i className="fa-brands fa-discord"></i>
+        </footer>
+    )
+}
