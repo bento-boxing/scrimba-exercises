16f1b918682b2d5bbc845164e53929a14f1fe64c
bento-boxing <superflyer7676@gmail.com>
2026-09-19 18:29:33 +0100

    style: style Header section.

--- 15business-card-react/src/components/Header.jsx
+++ 15business-card-react/src/components/Header.jsx
@@ -3,17 +3,17 @@ import reactLogoPath from '../assets/react.svg'
 export function Header() {
     return (
         <header className='header'>
-            <img src={reactLogoPath} alt='React Logo' />
-            <h1>bento-boxing</h1>
-            <h2>Frontend Developer</h2>
-            <a href='../../index.html'>bento-boxing.website</a>
+            <img className='header__image' src={reactLogoPath} alt='React Logo' />
+            <h1 className='header__name'>bento-boxing</h1>
+            <h2 className='header__title'>Frontend Developer</h2>
+            <a className='header__website' href='../../index.html'>bento-boxing.website</a>
 
-            <section className='links'>
-                <a href='mailto:bentoboxing@example123.com'>
+            <section className='header__links'>
+                <a className='links__link' href='mailto:bentoboxing@example123.com'>
                     <i className="fa-solid fa-envelope"></i>
                     Email
                 </a>
-                <a href='https://linkedin.com'>
+                <a className='links__link links__link--blue' href='https://linkedin.com'>
                     <i className="fa-brands fa-linkedin"></i>
                     LinkedIn
                 </a>
