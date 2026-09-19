abf05c961d70ab25638f8ccdffdd8999bd7ecd38
bento-boxing <superflyer7676@gmail.com>
2026-09-19 18:44:11 +0100

    feat: add links to social media icons.

--- 15business-card-react/src/components/Footer.jsx
+++ 15business-card-react/src/components/Footer.jsx
@@ -1,10 +1,18 @@
 export function Footer() {
     return (
         <footer className="footer">
-            <i className="fa-brands fa-instagram"></i>
-            <i className="fa-brands fa-facebook-f"></i>
-            <i className="fa-brands fa-twitter"></i>
-            <i className="fa-brands fa-discord"></i>
+            <a className='footer__link' href='https://instagram.com' target='_blank' aria-label="Instagram">
+                <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a className='footer__link' href='https://www.facebook.com' target='_blank' aria-label="Facebook">
+                <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a className='footer__link' href='https://www.twitter.com' target='_blank' aria-label="Twitter">
+                <i className="fa-brands fa-twitter"></i>
+            </a>
+            <a className='footer__link' href='https://www.discord.com' target='_blank' aria-label="Discord">
+                <i className="fa-brands fa-discord"></i>
+            </a>
         </footer>
     )
 }
