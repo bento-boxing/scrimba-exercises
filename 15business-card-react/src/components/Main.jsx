909066ae294663ed58781494a4b5de8f352a9756
bento-boxing <superflyer7676@gmail.com>
2026-09-19 18:05:23 +0100

    feat: Create Main.jsx

---
+++ 15business-card-react/src/components/Main.jsx
@@ -0,0 +1,21 @@
+export function Main() {
+    return (
+        <main className="main">
+            <section className='content-block'>
+                <h2 className='content-block__heading'>About</h2>
+                <p className='content-block__paragraph'>
+                    I am a frontend developer with a particular interest in making things simple and automating
+                    daily tasks. I try to keep up with security and best practices, and am always looking for new
+                    things to learn.
+                </p>
+            </section>
+            <section className='content-block'>
+                <h2 className='content-block__heading'>Interests</h2>
+                <p className='content-block__paragraph'>
+                    Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop
+                    culture ninja. Coffee fanatic.
+                </p>
+            </section>
+        </main>
+    )
+}
